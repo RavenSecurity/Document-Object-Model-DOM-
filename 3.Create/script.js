82f9f82f898acb5eaf9f65b20028b0b91c3a2982
RayaneKaddoury <rayanekaddoury@gmail.com>
2022-04-22 13:08:50 +0200

    Part 3 nearly done

--- 3.Create/script.js
+++ 3.Create/script.js
@@ -12,7 +12,27 @@ function generateRandomColor() {
 
 // generate eachn name's section
 
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
 let arr = ["Gilles", "Jamie", "Jason", "Jeremy", "Martin", "Emre", "Rayane", "Rayhan", "Ricardo", "Steffanie", "Sebastien", "Tanguy", "Thibaud", "Zakaria"];
+
+shuffle(arr)
 
 for (let elem of arr) {
     let nextSection = document.createElement("section");
